f1dfbda27d8805a5107dd3d22bcc3f26490e7215
Harshitha Basha <bashaharshitha@gmail.com>
2025-10-16 18:41:34 +0530

    Added new feature

--- src/assets/components/contactsection/ContactSocial.jsx
+++ src/assets/components/contactsection/ContactSocial.jsx
@@ -8,7 +8,7 @@ const ContactSocial = () => {
   return (
     <div className='flex gap-4'>
         <Singlecontact link='https://github.com/bashaharshitha-cmd' Icon={FaGithub}/>
-         <Singlecontact link="linkedin.com/in/harshitha-basha-9a6662222"/>
+         <Singlecontact link="https://www.linkedin.com/in/harshitha-basha-9a6662222?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" Icon={CiLinkedin}/>
           <Singlecontact link='https://www.instagram.com/life_is_wondderful?igsh=dWJiYnN6OHdmNGQ5&utm_source=ig_contact_invite' Icon={FaSquareInstagram}/>
     </div>
   );
